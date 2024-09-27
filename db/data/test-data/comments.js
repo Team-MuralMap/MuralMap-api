4ec3ba60d0785d1045d21118a5377336087b9128
xon-ac <shawn.clarke@icloud.com>
2024-09-27 15:25:48 +0100

    edited table names

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -1,8 +1,7 @@
 module.exports = [
     {
         body: 'nice',
-        Likes: 2,
-        author: Phillip6, 
+        author_id: 1, 
         post_id: 1,
         comment_id: 1,
         created_at: 1605107340000,
