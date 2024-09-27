e221be34c9980d4b6aa133120be722242cd6b840
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-09-27 15:57:45 +0100

    add more get tests and update properties

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,10 +15,31 @@ describe('GET /api/users', () => {
                 expect(user).toMatchObject({
                     user_id: expect.any(Number),
                     username: expect.any(String),
-                    avatar_url: expect.any(String)
+                    avatar_url: expect.any(String),
+                    name: expect.any(String)
                 });
             }
-        })
+        });
+    });
+});
+
+describe('GET /api/users/:user_id', () => {
+    test('returns user object with given ID and correct properties', () => {
+        return request(app)
+        .get('/api/users/1')
+        .expect(200)
+        .then((response) => {
+            const user = response.body.user;
+
+            expect(typeof user).toBe('object');
+
+            expect(user).toMatchObject({
+                user_id: 1,
+                username: expect.any(String),
+                avatar_url: expect.any(String),
+                name: expect.any(String)
+            });
+        });
     });
 });
 
@@ -37,10 +58,30 @@ describe('GET /api/sites', () => {
                     site_id: expect.any(Number),
                     latitude: expect.any(Number),
                     longitude: expect.any(Number),
-                    author: expect.any(String)
+                    author_id: expect.any(Number)
                 });
             }
-        })
+        });
+    });
+});
+
+describe('GET /api/sites/:site_id', () => {
+    test('returns site object with given ID and correct properties', () => {
+        return request(app)
+        .get('/api/sites/1')
+        .expect(200)
+        .then((response) => {
+            const site = response.body.site;
+
+            expect(typeof site).toBe('object');
+
+            expect(site).toMatchObject({
+                site_id: 1,
+                latitude: expect.any(Number),
+                longitude: expect.any(Number),
+                author_id: expect.any(Number)
+            });
+        });
     });
 });
 
@@ -58,7 +99,7 @@ describe('GET /api/posts', () => {
                 expect(post).toMatchObject({
                     post_id: expect.any(Number),
                     author: expect.any(String),
-                    photo_url: expect.any(String),
+                    img_url: expect.any(String),
                     latitude: expect.any(Number),
                     longitude: expect.any(Number),
                     body: expect.any(String),
@@ -66,7 +107,31 @@ describe('GET /api/posts', () => {
                     site_id: expect.any(Number)
                 });
             }
-        })
+        });
+    });
+});
+
+describe('GET /api/posts/:post_id', () => {
+    test('returns post object with given ID and correct properties', () => {
+        return request(app)
+        .get('/api/posts/1')
+        .expect(200)
+        .then((response) => {
+            const post = response.body.post;
+
+            expect(typeof post).toBe('object');
+
+            expect(post).toMatchObject({
+                post_id: 1,
+                author: expect.any(String),
+                img_url: expect.any(String),
+                latitude: expect.any(Number),
+                longitude: expect.any(Number),
+                body: expect.any(String),
+                created_at: expect.any(String),
+                site_id: expect.any(Number)
+            });
+        });
     });
 });
 
@@ -84,12 +149,34 @@ describe('GET /api/posts/:post_id/comments', () => {
                 expect(comment).toMatchObject({
                     comment_id: expect.any(Number),
                     author: expect.any(String),
-                    post_id: expect.any(Number),
+                    post_id: 1,
                     body: expect.any(String),
                     created_at: expect.any(String),
                     reply_to: expect.any(String)
                 });
             }
-        })
+        });
+    });
+});
+
+describe('GET /api/comments/:comment_id', () => {
+    test('returns comment object with given ID and correct properties', () => {
+        return request(app)
+        .get('/api/comments/1')
+        .expect(200)
+        .then((response) => {
+            const comment = response.body.comment;
+
+            expect(typeof comment).toBe('object');
+
+            expect(comment).toMatchObject({
+                comment_id: 1,
+                author: expect.any(String),
+                post_id: expect.any(Number),
+                body: expect.any(String),
+                created_at: expect.any(String),
+                reply_to: expect.any(String)
+            });
+        });
     });
 });
