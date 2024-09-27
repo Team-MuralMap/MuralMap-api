27793c21a96a7fd5b7205dee9c00cba5deae93ab
Jason Bell <jasonbell143@gmail.com>
2024-09-27 16:30:27 +0100

    updated some data keys

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -1,10 +1,10 @@
 module.exports = [
-    {
-        comment_id: 1,
-        body: 'nice',
-        user_id: 1, 
-        post_id: 1,
-        reply_to: 1,
-        created_at: 1605107340000,
-    }
-]
+  {
+    comment_id: 1,
+    body: "nice",
+    author_id: 1,
+    post_id: 1,
+    reply_to: 1,
+    created_at: 1605107340000,
+  },
+];
--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,10 +1,12 @@
 module.exports = [
-    {
-        user_id: 1,
-        user_name: 'Phillip6',
-        email: 'phillip6@gmail.com',
-        avatar_url: 'https://deadline.com/wp-content/uploads/2023/01/dr-phil-mcgraw-2.jpg?w=681&h=383&crop=1',
-        first_name: 'Phil',
-        surname: 'Phillips',
-    }
-]
+  {
+    user_id: 1,
+    username: "Phillip6",
+    email: "phillip6@gmail.com",
+    avatar_url:
+      "https://deadline.com/wp-content/uploads/2023/01/dr-phil-mcgraw-2.jpg?w=681&h=383&crop=1",
+    first_name: "Phil",
+    surname: "Phillips",
+    name: "Phil Phillips",
+  },
+];
