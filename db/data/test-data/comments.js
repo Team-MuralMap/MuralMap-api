5ec9f9f8f351391f816ba6877af9029843fdeee9
xon-ac <shawn.clarke@icloud.com>
2024-09-27 16:35:56 +0100

    added more test data

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -6,5 +6,13 @@ module.exports = [
         post_id: 1,
         reply_to: 1,
         created_at: 1605107340000,
-    }
+    },
+    {
+        comment_id: 1,
+        body: "i've been there",
+        user_id: 2, 
+        post_id: 2,
+        reply_to: 1,
+        created_at: 1605107340000,
+    },
 ]
