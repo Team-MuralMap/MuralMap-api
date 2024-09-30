4f0bd14337a0c5b85da76d6c7e27646d647ddef1
charlie-1510 <chanp1992@googlemail.com>
2024-09-30 16:26:06 +0100

    changed first and last name to just name

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -5,8 +5,7 @@ module.exports = [
     email: "phillip6@gmail.com",
     avatar_url:
       "https://deadline.com/wp-content/uploads/2023/01/dr-phil-mcgraw-2.jpg?w=681&h=383&crop=1",
-    first_name: "Phil",
-    surname: "Phillips",
+    name: "Phil Phillips",
   },
   {
     user_id: 2,
@@ -14,8 +13,7 @@ module.exports = [
     email: "staceyjones@gmail.com",
     avatar_url:
       "https://hackspirit.com/wp-content/uploads/2021/06/Copy-of-Rustic-Female-Teen-Magazine-Cover.jpg",
-    first_name: "Stacey",
-    surname: "Jones",
+    name: "Stacey Jones",
   },
   {
     user_id: 3,
@@ -23,7 +21,6 @@ module.exports = [
     email: "janjanjan2008@gmail.com",
     avatar_url:
       "https://www.verywellmind.com/thmb/0-BmnBjLqPp0967GRFRzwuB6n1o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1311372339-af583a5aa2ab46a1b884b961daca5232.jpg",
-    first_name: "Jan",
-    surname: "Peterage",
+    name: "Jan Peterage",
   },
 ];
