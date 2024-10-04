84bf20739a76e73547a6f6730b85accf45fa861e
charlie-1510 <chanp1992@googlemail.com>
2024-10-04 10:23:39 +0100

    installed cors and updated endpoints

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
 const apiRouter = require("./routes/api-router");
 
-//  app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
