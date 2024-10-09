1c855790fa9f958dc3c190dee9592d68e32fa55c
Jason Bell <jasonbell143@gmail.com>
2024-10-09 15:26:46 +0100

    add dev data for posts and sites, with sharing of posts for scrolling through

--- db/data/development-data/sites.js
+++ db/data/development-data/sites.js
@@ -1,62 +1,211 @@
 module.exports = [
-  { // batman and superman kiss (Manchester)
+  {
+    // batman and superman kiss (Manchester)
     site_id: 1,
     user_id: 1,
     longitude: -2.238269,
     latitude: 53.476267,
   },
-  { // Japanese lady (Lisbon)
+  {
+    // Japanese lady (Lisbon)
     site_id: 2,
     user_id: 1,
     longitude: -9.129871,
     latitude: 38.71154,
   },
-  { // owls (Manchester)
+  {
+    // owls (Manchester)
     site_id: 3,
     user_id: 1,
     longitude: -2.263382,
     latitude: 53.459629,
   },
-  { // man playing guitar (Lisbon)
+  {
+    // man playing guitar (Lisbon)
     site_id: 4,
     user_id: 1,
     longitude: -9.135983,
     latitude: 38.712745,
   },
-  { // guy at table (Lisbon) - approx
+  {
+    // guy at table (Lisbon) - approx
     site_id: 5,
     user_id: 3,
-    longitude: -9.133060,
+    longitude: -9.13306,
     latitude: 38.713014,
   },
-  { // deer with drink (Saragosse)
+  {
+    // deer with drink (Saragosse)
     site_id: 6,
     user_id: 3,
     longitude: -0.884506,
     latitude: 41.656412,
   },
-  { // colourful woman (Kettering)
+  {
+    // colourful woman (Kettering)
     site_id: 7,
     user_id: 1,
-    longitude: -0.726970,
+    longitude: -0.72697,
     latitude: 52.398476,
   },
-  { // Leicester City FC mural (Leicester)
+  {
+    // Leicester City FC mural (Leicester)
     site_id: 8,
     user_id: 1,
     longitude: -1.138714,
     latitude: 52.631792,
   },
-  { // Ian Curtis (Manchester)
+  {
+    // Ian Curtis (Manchester)
     site_id: 9,
     user_id: 3,
     longitude: -2.227158,
     latitude: 53.476108,
   },
-  { // Alan Turing (Manchester)
+  {
+    // Alan Turing (Manchester)
     site_id: 10,
     user_id: 4,
     longitude: -2.254513,
     latitude: 53.425249,
+  },
+  {
+    // Stevenson Square
+    site_id: 11,
+    user_id: 4,
+    latitude: 53.48268728223822,
+    longitude: -2.2340893658270686,
+  },
+  {
+    site_id: 12,
+    user_id: 4,
+    longitude: -2.332086,
+    latitude: 53.485358,
+  },
+  {
+    site_id: 13,
+    user_id: 4,
+    longitude: -2.297731,
+    latitude: 53.464924,
+  },
+  {
+    site_id: 14,
+    user_id: 2,
+    longitude: -2.253756,
+    latitude: 53.441209,
+  },
+  {
+    site_id: 15,
+    user_id: 2,
+    longitude: -2.223524,
+    latitude: 53.428937,
+  },
+  {
+    site_id: 16,
+    user_id: 3,
+    longitude: -2.261315,
+    latitude: 53.516806,
+  },
+  {
+    site_id: 17,
+    user_id: 3,
+    longitude: -2.189169,
+    latitude: 53.510274,
+  },
+  {
+    site_id: 18,
+    user_id: 4,
+    longitude: -2.145881,
+    latitude: 53.45266,
+  },
+  {
+    site_id: 19,
+    user_id: 2,
+    longitude: -2.246198,
+    latitude: 53.425255,
+  },
+  {
+    site_id: 20,
+    user_id: 2,
+    longitude: -2.248947,
+    latitude: 53.542928,
+  },
+  {
+    site_id: 21,
+    user_id: 3,
+    longitude: -2.266159,
+    latitude: 53.485767,
+  },
+  {
+    site_id: 22,
+    user_id: 4,
+    longitude: -2.427629,
+    latitude: 53.581264,
+  },
+  {
+    site_id: 23,
+    user_id: 2,
+    longitude: -2.206381,
+    latitude: 53.551087,
+  },
+  {
+    site_id: 24,
+    user_id: 2,
+    longitude: -2.211191,
+    latitude: 53.393739,
+  },
+  {
+    site_id: 25,
+    user_id: 1,
+    longitude: -2.100567,
+    latitude: 53.475551,
+  },
+  {
+    site_id: 26,
+    user_id: 1,
+    longitude: -2.169277,
+    latitude: 53.413388,
+  },
+  {
+    site_id: 27,
+    user_id: 4,
+    longitude: -2.347924,
+    latitude: 53.390463,
+  },
+  {
+    site_id: 28,
+    user_id: 3,
+    longitude: -2.233865,
+    latitude: 53.331456,
+  },
+  {
+    site_id: 29,
+    user_id: 4,
+    longitude: -2.289521,
+    latitude: 53.551087,
+  },
+  {
+    site_id: 30,
+    user_id: 4,
+    longitude: -2.154161,
+    latitude: 53.619973,
+  },
+  {
+    site_id: 31,
+    user_id: 1,
+    longitude: -2.369921,
+    latitude: 53.303153,
+  },
+  {
+    site_id: 32,
+    user_id: 3,
+    longitude: -2.082714,
+    latitude: 53.454704,
+  },
+  {
+    site_id: 33,
+    user_id: 3,
+    longitude: -2.21327,
+    latitude: 53.52742,
   },
 ];
